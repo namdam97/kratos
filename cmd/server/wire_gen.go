0d5106667e33de6b2c85c92aea11c23a837278c6
namdt <namdamthuan@vccorp.vn>
2023-12-14 23:48:02 +0700

    init run and debug server

--- cmd/server/wire_gen.go
+++ cmd/server/wire_gen.go
@@ -7,14 +7,17 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
 	"server/internal/biz"
 	"server/internal/conf"
 	"server/internal/data"
 	"server/internal/server"
 	"server/internal/service"
+)
 
-	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
+import (
+	_ "go.uber.org/automaxprocs"
 )
 
 // Injectors from wire.go:
